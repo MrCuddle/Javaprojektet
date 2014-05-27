7ec30c364791b06a69dc8c9f76256c45e71f643a
MrCuddle <simondahlberg89@gmail.com>
2014-05-27 17:24:51 +0200

    YAY I'M USING GIT
    HEHU

---
+++ Databasprojektet/src/databasprojektet/Application.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2014 Simon Dahlberg and Jesper Sahlin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package databasprojektet;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Simon Dahlberg and Jesper Sahlin
+ */
+public class Application{
+    private final String lookAndFeelPath = "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel";
+    private MainWindow mainWindow;
+    public Application()
+    {
+        Initialize();
+        
+    }
+
+    private void Initialize() {
+        try
+        {
+            UIManager.setLookAndFeel(lookAndFeelPath);
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void Run()
+    {
+        System.out.println("Started");        
+        
+        ConnectToDatabase();
+        mainWindow = new MainWindow();
+        
+        mainWindow.setVisible(true);
+        
+        
+    }
+
+    private void ConnectToDatabase()
+    {
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                 SQLHelper.Connect();
+                } 
+                catch (SQLException e) 
+                {
+                e.printStackTrace();
+                }
+                
+                mainWindow.AppendStatusWindow(SQLHelper.GetStatus());
+            }
+        }.start();
+    }
+    
+}
---
+++ Databasprojektet/src/databasprojektet/SQLHelper.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2014 Simon Dahlberg and Jesper Sahlin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package databasprojektet;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Simon Dahlberg and Jesper Sahlin
+ */
+public class SQLHelper {
+    private static Connection con;
+    private static Statement statement;
+    private static final String hostIP = "195.178.232.7", hostPort = "4040";
+    private static final String connectionFormat = "jdbc:mysql://" + hostIP + ":" + hostPort + "/username";
+    private static String status;
+    private static String userName, userPass;
+    
+    public static boolean Connect() throws SQLException
+    {
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            
+        } 
+        catch (ClassNotFoundException e) 
+        {
+            status = "Databas-driver hittades ej " + e;
+            System.out.println(status);
+            return false;
+        }
+        
+        try 
+            {
+                con = DriverManager.getConnection(connectionFormat,"username","password");
+                statement = con.createStatement();
+            } catch (SQLException e) {
+                status = "Kunde inte ansluta till databasen " + e;
+                System.out.println(status);
+                return false;
+            } 
+        status = "Ansluten till databasen";
+        return true;
+    }
+    
+    public static boolean Disconnect()
+    {
+        if(con != null)
+        {
+            try {
+                con.close();
+                status = "Ej ansluten";
+            } catch (SQLException e) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+        
+    }
+    
+    //returnerar huruvida con finns och con.close() INTE blivit kallad
+    //observera att anslutningen fortfarande kan ha avbrutits på annat sätt
+    public static boolean IsConnected()
+    {
+        if(con != null)
+        {
+            try {
+                return !con.isClosed();
+            }
+            catch (SQLException e) 
+            {
+                System.out.println(e);
+            }
+        }       
+        return false;
+        
+    }
+    
+    public static void SendSQLQuery(String comman)
+    {
+        if(IsConnected())
+        {
+            ///Do SQL
+            
+        }
+    }
+    
+    public static String GetStatus()
+    {
+        return status;
+    }
+    
+            
+    
+}
